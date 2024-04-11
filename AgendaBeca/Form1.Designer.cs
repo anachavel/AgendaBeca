48daf2ad979dad7180e54767b163261b876df75f
AVANADE-CORP\ana.chavez.velasco <ana.chavez.velasco@gmail.com>
2024-04-11 11:59:03 +0200

    Agrego las comprobaciones y la conexión a la base de datos

--- AgendaBeca/Form1.Designer.cs
+++ AgendaBeca/Form1.Designer.cs
@@ -28,12 +28,221 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            buttonGuardar = new Button();
+            buttonCancelar = new Button();
+            buttonEliminar = new Button();
+            buttonAnyadir = new Button();
+            buttonModificar = new Button();
+            dataGridViewDatos = new DataGridView();
+            groupBoxContacto = new GroupBox();
+            lblId = new Label();
+            textBoxId = new TextBox();
+            lblObservaciones = new Label();
+            textBoxObservaciones = new TextBox();
+            lblTelefono = new Label();
+            textBoxTelefono = new TextBox();
+            lblFechaNacimiento = new Label();
+            dateTimePickerFechaNacimiento = new DateTimePicker();
+            lblNombre = new Label();
+            textBoxNombre = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewDatos).BeginInit();
+            groupBoxContacto.SuspendLayout();
+            SuspendLayout();
+            // 
+            // buttonGuardar
+            // 
+            buttonGuardar.Location = new Point(556, 338);
+            buttonGuardar.Name = "buttonGuardar";
+            buttonGuardar.Size = new Size(112, 34);
+            buttonGuardar.TabIndex = 10;
+            buttonGuardar.Text = "Guardar";
+            buttonGuardar.UseVisualStyleBackColor = true;
+            buttonGuardar.Click += buttonGuardar_Click;
+            // 
+            // buttonCancelar
+            // 
+            buttonCancelar.Location = new Point(694, 338);
+            buttonCancelar.Name = "buttonCancelar";
+            buttonCancelar.Size = new Size(112, 34);
+            buttonCancelar.TabIndex = 11;
+            buttonCancelar.Text = "Cancelar";
+            buttonCancelar.UseVisualStyleBackColor = true;
+            // 
+            // buttonEliminar
+            // 
+            buttonEliminar.Location = new Point(216, 338);
+            buttonEliminar.Name = "buttonEliminar";
+            buttonEliminar.Size = new Size(112, 34);
+            buttonEliminar.TabIndex = 13;
+            buttonEliminar.Text = "Eliminar";
+            buttonEliminar.UseVisualStyleBackColor = true;
+            // 
+            // buttonAnyadir
+            // 
+            buttonAnyadir.Location = new Point(78, 338);
+            buttonAnyadir.Name = "buttonAnyadir";
+            buttonAnyadir.Size = new Size(112, 34);
+            buttonAnyadir.TabIndex = 12;
+            buttonAnyadir.Text = "Añadir";
+            buttonAnyadir.UseVisualStyleBackColor = true;
+            // 
+            // buttonModificar
+            // 
+            buttonModificar.Location = new Point(356, 338);
+            buttonModificar.Name = "buttonModificar";
+            buttonModificar.Size = new Size(112, 34);
+            buttonModificar.TabIndex = 14;
+            buttonModificar.Text = "Modificar";
+            buttonModificar.UseVisualStyleBackColor = true;
+            buttonModificar.Click += button3_Click;
+            // 
+            // dataGridViewDatos
+            // 
+            dataGridViewDatos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewDatos.Location = new Point(69, 391);
+            dataGridViewDatos.Name = "dataGridViewDatos";
+            dataGridViewDatos.RowHeadersWidth = 62;
+            dataGridViewDatos.RowTemplate.Height = 33;
+            dataGridViewDatos.Size = new Size(748, 198);
+            dataGridViewDatos.TabIndex = 15;
+            // 
+            // groupBoxContacto
+            // 
+            groupBoxContacto.Controls.Add(lblId);
+            groupBoxContacto.Controls.Add(textBoxId);
+            groupBoxContacto.Controls.Add(lblObservaciones);
+            groupBoxContacto.Controls.Add(textBoxObservaciones);
+            groupBoxContacto.Controls.Add(lblTelefono);
+            groupBoxContacto.Controls.Add(textBoxTelefono);
+            groupBoxContacto.Controls.Add(lblFechaNacimiento);
+            groupBoxContacto.Controls.Add(dateTimePickerFechaNacimiento);
+            groupBoxContacto.Controls.Add(lblNombre);
+            groupBoxContacto.Controls.Add(textBoxNombre);
+            groupBoxContacto.Location = new Point(69, 36);
+            groupBoxContacto.Name = "groupBoxContacto";
+            groupBoxContacto.Size = new Size(737, 296);
+            groupBoxContacto.TabIndex = 16;
+            groupBoxContacto.TabStop = false;
+            groupBoxContacto.Text = "Contacto:";
+            // 
+            // lblId
+            // 
+            lblId.AutoSize = true;
+            lblId.Location = new Point(39, 33);
+            lblId.Name = "lblId";
+            lblId.Size = new Size(32, 25);
+            lblId.TabIndex = 19;
+            lblId.Text = "Id:";
+            // 
+            // textBoxId
+            // 
+            textBoxId.Location = new Point(223, 30);
+            textBoxId.Name = "textBoxId";
+            textBoxId.Size = new Size(91, 31);
+            textBoxId.TabIndex = 18;
+            // 
+            // lblObservaciones
+            // 
+            lblObservaciones.AutoSize = true;
+            lblObservaciones.Location = new Point(39, 198);
+            lblObservaciones.Name = "lblObservaciones";
+            lblObservaciones.Size = new Size(132, 25);
+            lblObservaciones.TabIndex = 17;
+            lblObservaciones.Text = "Observaciones:";
+            // 
+            // textBoxObservaciones
+            // 
+            textBoxObservaciones.Location = new Point(223, 198);
+            textBoxObservaciones.Multiline = true;
+            textBoxObservaciones.Name = "textBoxObservaciones";
+            textBoxObservaciones.Size = new Size(438, 88);
+            textBoxObservaciones.TabIndex = 16;
+            // 
+            // lblTelefono
+            // 
+            lblTelefono.AutoSize = true;
+            lblTelefono.Location = new Point(39, 159);
+            lblTelefono.Name = "lblTelefono";
+            lblTelefono.Size = new Size(83, 25);
+            lblTelefono.TabIndex = 15;
+            lblTelefono.Text = "Teléfono:";
+            // 
+            // textBoxTelefono
+            // 
+            textBoxTelefono.Location = new Point(223, 156);
+            textBoxTelefono.Name = "textBoxTelefono";
+            textBoxTelefono.Size = new Size(186, 31);
+            textBoxTelefono.TabIndex = 14;
+            // 
+            // lblFechaNacimiento
+            // 
+            lblFechaNacimiento.AutoSize = true;
+            lblFechaNacimiento.Location = new Point(39, 115);
+            lblFechaNacimiento.Name = "lblFechaNacimiento";
+            lblFechaNacimiento.Size = new Size(178, 25);
+            lblFechaNacimiento.TabIndex = 13;
+            lblFechaNacimiento.Text = "Fecha de nacimiento:";
+            // 
+            // dateTimePickerFechaNacimiento
+            // 
+            dateTimePickerFechaNacimiento.Location = new Point(223, 110);
+            dateTimePickerFechaNacimiento.Name = "dateTimePickerFechaNacimiento";
+            dateTimePickerFechaNacimiento.Size = new Size(300, 31);
+            dateTimePickerFechaNacimiento.TabIndex = 12;
+            // 
+            // lblNombre
+            // 
+            lblNombre.AutoSize = true;
+            lblNombre.Location = new Point(39, 70);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new Size(82, 25);
+            lblNombre.TabIndex = 11;
+            lblNombre.Text = "Nombre:";
+            // 
+            // textBoxNombre
+            // 
+            textBoxNombre.Location = new Point(223, 67);
+            textBoxNombre.Name = "textBoxNombre";
+            textBoxNombre.Size = new Size(272, 31);
+            textBoxNombre.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(890, 622);
+            Controls.Add(groupBoxContacto);
+            Controls.Add(dataGridViewDatos);
+            Controls.Add(buttonModificar);
+            Controls.Add(buttonEliminar);
+            Controls.Add(buttonAnyadir);
+            Controls.Add(buttonCancelar);
+            Controls.Add(buttonGuardar);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridViewDatos).EndInit();
+            groupBoxContacto.ResumeLayout(false);
+            groupBoxContacto.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+        private Button buttonGuardar;
+        private Button buttonCancelar;
+        private Button buttonEliminar;
+        private Button buttonAnyadir;
+        private Button buttonModificar;
+        private DataGridView dataGridViewDatos;
+        private GroupBox groupBoxContacto;
+        private Label lblId;
+        private TextBox textBoxId;
+        private Label lblObservaciones;
+        private TextBox textBoxObservaciones;
+        private Label lblTelefono;
+        private TextBox textBoxTelefono;
+        private Label lblFechaNacimiento;
+        private DateTimePicker dateTimePickerFechaNacimiento;
+        private Label lblNombre;
+        private TextBox textBoxNombre;
     }
 }
--- AgendaBeca/Form1.cs
+++ AgendaBeca/Form1.cs
@@ -1,10 +1,67 @@
+using System.Data.SqlClient;
+
 namespace AgendaBeca
 {
     public partial class Form1 : Form
     {
+        private string conexionBD = "Server=(local);Database=AgendaBeca;Trusted_Connection=True;";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonGuardar_Click(object sender, EventArgs e)
+        {
+            String nombre = textBoxNombre.Text;
+            DateTime fechaNacimiento = dateTimePickerFechaNacimiento.Value;
+            String telefono = textBoxTelefono.Text;
+            String observaciones = textBoxObservaciones.Text;
+
+            if (nombre.Length > 100)
+            {
+                MessageBox.Show("El nombre no puede tener más de 100 caracteres.");
+                return;
+            }
+
+            if (telefono.Length != 9)
+            {
+                MessageBox.Show("El teléfono debe de tener exactamente 9 caracteres.");
+                return;
+            }
+
+            if (observaciones.Length > 500)
+            {
+                MessageBox.Show("Las observaciones no pueden tener más de 500 caracteres.");
+                return;
+            }
+
+                MessageBox.Show("Contacto registrado.");
         }
     }
 }
